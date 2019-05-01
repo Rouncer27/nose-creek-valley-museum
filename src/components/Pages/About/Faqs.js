2c9d7c5886ec7c212f71c50e1454e3a7e79b0bba
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-05-01 17:09:17 -0600

    work on the about page

--- src/components/Pages/About/Faqs.js
+++ src/components/Pages/About/Faqs.js
@@ -1,12 +1,133 @@
 import React, { Component } from "react"
+import styled from "styled-components"
+
+import { StandardWrapper } from "../../../components/styles/Commons/Wrappers"
+
+const FaqsStyled = styled.section`
+  padding: 5rem 0;
+  background-color: #eaeaea;
+
+  .faqs__title {
+    width: 100%;
+    text-align: left;
+    h2 {
+      color: ${props => props.theme.deepSea};
+      font-family: ${props => props.theme.fontSec};
+      line-height: 1.25;
+
+      @media (min-width: ${props => props.theme.bpDesksm}) {
+        font-size: 7.5rem;
+      }
+    }
+  }
+
+  .faqs__container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+  }
+
+  .faqs__item {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+
+    &--question {
+      display: flex;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      justify-content: center;
+      @media (min-width: ${props => props.theme.bpTablet}) {
+        width: calc(50% - 4rem);
+        margin: 2rem;
+      }
+
+      &--title {
+        width: 15%;
+        margin: 0;
+        color: ${props => props.theme.neptune};
+        font-family: ${props => props.theme.fontSec};
+        font-size: 3rem;
+        line-height: 1;
+
+        @media (min-width: ${props => props.theme.bpTablet}) {
+          font-size: 4rem;
+        }
+      }
+
+      &--content {
+        width: 85%;
+        margin: 0;
+      }
+    }
+
+    &--answer {
+      display: flex;
+      align-items: flex-start;
+      flex-wrap: wrap;
+      justify-content: center;
+      @media (min-width: ${props => props.theme.bpTablet}) {
+        width: calc(50% - 4rem);
+        margin: 2rem;
+      }
+
+      &--title {
+        width: 100%;
+        margin: 0;
+        color: ${props => props.theme.neptune};
+        font-family: ${props => props.theme.fontSec};
+        font-size: 3rem;
+        line-height: 1;
+
+        @media (min-width: ${props => props.theme.bpTablet}) {
+          font-size: 4rem;
+        }
+      }
+
+      &--content {
+        width: 100%;
+
+        p {
+          margin: 0;
+        }
+      }
+    }
+  }
+`
 
 class Faqs extends Component {
   render() {
-    console.log(this.props)
     return (
-      <div>
-        <h1>Faqs</h1>
-      </div>
+      <FaqsStyled className="faqs">
+        <StandardWrapper className="faqs__wrapper">
+          <div className="faqs__title">
+            <h2>Faqs</h2>
+          </div>
+          <div className="faqs__container">
+            {this.props.faqs.map((faq, index) => {
+              return (
+                <div key={index} className="faqs__item">
+                  <div className="faqs__item--question">
+                    <p className="faqs__item--question--title">Q</p>
+                    <p className="faqs__item--question--content">
+                      {faq.question}
+                    </p>
+                  </div>
+                  <div className="faqs__item--answer">
+                    <p className="faqs__item--answer--title">Answer</p>
+                    <div
+                      className="faqs__item--answer--content"
+                      dangerouslySetInnerHTML={{ __html: faq.answer }}
+                    />
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </StandardWrapper>
+      </FaqsStyled>
     )
   }
 }
--- src/components/Pages/About/ThankYou.js
+++ src/components/Pages/About/ThankYou.js
@@ -1,12 +1,128 @@
 import React, { Component } from "react"
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+const ThankYouStyled = styled.section`
+  position: relative;
+  margin-bottom: 15rem;
+  z-index: 10;
+
+  .thankyou__wrapper {
+    display: flex;
+    position: relative;
+    flex-wrap: wrap;
+    justify-content: center;
+    width: 100%;
+    max-width: 45rem;
+    margin: ;
+    padding: 2rem;
+    padding-top: 0;
+
+    @media (min-width: ${props => props.theme.bpTablet}) {
+      max-width: 70rem;
+    }
+
+    @media (min-width: ${props => props.theme.bpDesksm}) {
+      max-width: 100rem;
+    }
+  }
+
+  .thankyou__container {
+    position: relative;
+    width: 100%;
+    max-width: 45rem;
+    height: 45rem;
+    margin-right: 5rem;
+    margin-left: 5rem;
+
+    @media (min-width: ${props => props.theme.bpTablet}) {
+      max-width: 70rem;
+      height: 70rem;
+    }
+
+    @media (min-width: ${props => props.theme.bpDesksm}) {
+      max-width: 90rem;
+      height: 50rem;
+    }
+
+    &--image {
+      width: 100%;
+
+      .gatsby-image-wrapper {
+        position: absolute !important;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+
+        img {
+          object-fit: cover;
+          object-position: center center;
+        }
+      }
+    }
+
+    &--content {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      justify-content: center;
+      position: absolute;
+      right: 0;
+      bottom: 0;
+      max-width: 50%;
+      padding: 5rem 2rem;
+      background: ${props => props.theme.grape};
+    }
+
+    h2 {
+      width: 100%;
+      margin: 0;
+      margin-bottom: 1rem;
+      color: ${props => props.theme.deco};
+      font-family: ${props => props.theme.fontSec};
+      font-style: italic;
+
+      @media (min-width: ${props => props.theme.bpDesksm}) {
+        font-size: 1.6rem;
+      }
+    }
+
+    &--paragraphs {
+      p {
+        margin: 0;
+        color: ${props => props.theme.white};
+        font-family: ${props => props.theme.fontTer};
+        font-weight: 300;
+        opacity: 0.8;
+
+        @media (min-width: ${props => props.theme.bpDesksm}) {
+          font-size: 1.6rem;
+        }
+      }
+    }
+  }
+`
 
 class ThankYou extends Component {
   render() {
-    console.log(this.props)
     return (
-      <div>
-        <h1>ThankYou</h1>
-      </div>
+      <ThankYouStyled className="thankyou">
+        <div className="thankyou__wrapper">
+          <div className="thankyou__container">
+            <div className="thankyou__container--image">
+              <Img fluid={this.props.imgFluid} alt={this.props.imgAlt} />
+            </div>
+            <div className="thankyou__container--content">
+              <h2>/ Thank you</h2>
+              <div
+                className="thankyou__container--paragraphs"
+                dangerouslySetInnerHTML={{ __html: this.props.mainContent }}
+              />
+            </div>
+          </div>
+        </div>
+      </ThankYouStyled>
     )
   }
 }
