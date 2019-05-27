b9bb634ceae510e46a9140feea0ba715fb8dec12
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-05-27 15:12:55 -0600

    form styles

--- src/components/Pages/Programs/FormSentSuccess.js
+++ src/components/Pages/Programs/FormSentSuccess.js
@@ -25,10 +25,83 @@ const FormSentSuccessStyle = styled.div`
     }
 
     &--button {
-      display: block;
-      margin-top: 4rem;
-      margin-right: auto;
-      margin-left: auto;
+      display: inline-block;
+      position: relative;
+      padding: 1rem 0;
+      transition: all 0.3s ease-in-out;
+      background: transparent;
+      color: ${props => props.theme.grape};
+      border: none;
+      text-transform: uppercase;
+
+      @media (min-width: ${props => props.theme.bpTablet}) {
+      }
+
+      @media (min-width: ${props => props.theme.bpDesksm}) {
+        margin-bottom: 1rem;
+        font-size: 1.8rem;
+      }
+
+      .italic-btn {
+        font-family: ${props => props.theme.fontSec};
+        font-style: italic;
+        text-transform: capitalize;
+      }
+
+      .btn-circle {
+        display: block;
+        position: absolute;
+        top: 50%;
+        right: -4.5rem;
+        width: 3.2rem;
+        height: 3.2rem;
+        transform: translate(0%, -50%);
+        transition: all 0.3s ease-in-out;
+        color: ${props => props.theme.grape};
+        text-align: center;
+
+        &::before {
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          width: 100%;
+          height: 100%;
+          transform: translate(-50%, -50%);
+          transition: all 0.3s ease-in-out;
+          border: 0.2rem solid ${props => props.theme.grape};
+          border-radius: 50%;
+          content: "";
+        }
+
+        &::after {
+          display: block;
+          position: absolute;
+          top: 50%;
+          left: 0%;
+          transform: translate(0%, -50%);
+          transition: all 0.3s ease-in-out;
+          font-family: ${props => props.theme.fontAwesome};
+          font-size: 2.2rem;
+          font-weight: 100;
+          content: "\f178";
+        }
+      }
+
+      &:hover {
+        color: ${props => props.theme.kenyanCopper};
+        cursor: pointer;
+
+        .btn-circle {
+          color: ${props => props.theme.kenyanCopper};
+          &::before {
+            border-color: ${props => props.theme.kenyanCopper};
+          }
+          &::after {
+            color: ${props => props.theme.kenyanCopper};
+          }
+        }
+      }
     }
   }
 
@@ -53,7 +126,8 @@ const FormSentSuccess = ({ resetTheForm }) => {
           onClick={resetTheForm}
           className="form-send-modal__message--button"
         >
-          Close
+          <span className="italic-btn">Close</span>
+          <span className="btn-circle" />
         </button>
       </div>
       <div className="form-send-modal__background" />
