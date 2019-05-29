021c410e335e93f9d8c04b02d06956edfec7f8fb
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-05-29 16:05:16 -0600

    bug in the gallery

--- src/components/Pages/Programs/useMedia.js
+++ src/components/Pages/Programs/useMedia.js
@@ -5,10 +5,13 @@ export default function useMedia(queries, values, defaultValue) {
     const match = () =>
       values[queries.findIndex(q => matchMedia(q).matches)] || defaultValue
     const [value, set] = useState(match)
+
     useEffect(() => {
       const handler = () => set(match)
       window.addEventListener("resize", handler)
-      return () => window.removeEventListener("resize", handler)
+      return () => {
+        window.removeEventListener("resize", handler)
+      }
     }, [])
     return value
   } else {
