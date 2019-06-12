b76d7d916cbe985a2c80ad537814a444199a6286
Trevor Rounce <trevorrounce@Trevors-iMac.local>
2019-06-12 13:26:54 -0600

    fixed the stories

--- src/components/Pages/Stories/RowFive.js
+++ src/components/Pages/Stories/RowFive.js
@@ -28,66 +28,79 @@ class RowFive extends Component {
     return (
       <RowFiveStyled>
         <div className="rfive__container">
-          <StoryCardSmall
-            to={`/stories/${this.props.postnine.node.slug}`}
-            className="rfive__atriclenine"
-          >
-            <div className="rfive__atriclenine--image storyCardImage">
-              <Img
-                fluid={
-                  this.props.postnine.node.acf._ncvm_featured_image.localFile
-                    .childImageSharp.fluid
-                }
-                alt={this.props.postnine.node.acf._ncvm_featured_image.alt_text}
-              />
-            </div>
-            <div className="rfive__atriclenine--title storyCardTitle">
-              <p>Written By {this.props.postnine.node.acf._ncvm_post_author}</p>
-              <h2>{this.props.postnine.node.title}</h2>
-            </div>
-          </StoryCardSmall>
+          {this.props.postnine !== undefined && (
+            <StoryCardSmall
+              to={`/stories/${this.props.postnine.node.slug}`}
+              className="rfive__atriclenine"
+            >
+              <div className="rfive__atriclenine--image storyCardImage">
+                <Img
+                  fluid={
+                    this.props.postnine.node.acf._ncvm_featured_image.localFile
+                      .childImageSharp.fluid
+                  }
+                  alt={
+                    this.props.postnine.node.acf._ncvm_featured_image.alt_text
+                  }
+                />
+              </div>
+              <div className="rfive__atriclenine--title storyCardTitle">
+                <p>
+                  Written By {this.props.postnine.node.acf._ncvm_post_author}
+                </p>
+                <h2>{this.props.postnine.node.title}</h2>
+              </div>
+            </StoryCardSmall>
+          )}
 
-          <StoryCardSmall
-            to={`/stories/${this.props.postTen.node.slug}`}
-            className="rfive__atricleten"
-          >
-            <div className="rfive__atricleten--image storyCardImage">
-              <Img
-                fluid={
-                  this.props.postTen.node.acf._ncvm_featured_image.localFile
-                    .childImageSharp.fluid
-                }
-                alt={this.props.postTen.node.acf._ncvm_featured_image.alt_text}
-              />
-            </div>
-            <div className="rfive__atricleten--title storyCardTitle">
-              <p>Written By {this.props.postTen.node.acf._ncvm_post_author}</p>
-              <h2>{this.props.postTen.node.title}</h2>
-            </div>
-          </StoryCardSmall>
-
-          <StoryCardMedium
-            to={`/stories/${this.props.postEleven.node.slug}`}
-            className="rfive__atricleeleven"
-          >
-            <div className="rfive__atricleeleven--image storyCardImage">
-              <Img
-                fluid={
-                  this.props.postEleven.node.acf._ncvm_featured_image.localFile
-                    .childImageSharp.fluid
-                }
-                alt={
-                  this.props.postEleven.node.acf._ncvm_featured_image.alt_text
-                }
-              />
-            </div>
-            <div className="rfive__atricleeleven--title storyCardTitle">
-              <p>
-                Written By {this.props.postEleven.node.acf._ncvm_post_author}
-              </p>
-              <h2>{this.props.postEleven.node.title}</h2>
-            </div>
-          </StoryCardMedium>
+          {this.props.postTen !== undefined && (
+            <StoryCardSmall
+              to={`/stories/${this.props.postTen.node.slug}`}
+              className="rfive__atricleten"
+            >
+              <div className="rfive__atricleten--image storyCardImage">
+                <Img
+                  fluid={
+                    this.props.postTen.node.acf._ncvm_featured_image.localFile
+                      .childImageSharp.fluid
+                  }
+                  alt={
+                    this.props.postTen.node.acf._ncvm_featured_image.alt_text
+                  }
+                />
+              </div>
+              <div className="rfive__atricleten--title storyCardTitle">
+                <p>
+                  Written By {this.props.postTen.node.acf._ncvm_post_author}
+                </p>
+                <h2>{this.props.postTen.node.title}</h2>
+              </div>
+            </StoryCardSmall>
+          )}
+          {this.props.postEleven !== undefined && (
+            <StoryCardMedium
+              to={`/stories/${this.props.postEleven.node.slug}`}
+              className="rfive__atricleeleven"
+            >
+              <div className="rfive__atricleeleven--image storyCardImage">
+                <Img
+                  fluid={
+                    this.props.postEleven.node.acf._ncvm_featured_image
+                      .localFile.childImageSharp.fluid
+                  }
+                  alt={
+                    this.props.postEleven.node.acf._ncvm_featured_image.alt_text
+                  }
+                />
+              </div>
+              <div className="rfive__atricleeleven--title storyCardTitle">
+                <p>
+                  Written By {this.props.postEleven.node.acf._ncvm_post_author}
+                </p>
+                <h2>{this.props.postEleven.node.title}</h2>
+              </div>
+            </StoryCardMedium>
+          )}
         </div>
       </RowFiveStyled>
     )
