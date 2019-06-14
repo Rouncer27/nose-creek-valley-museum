22f807690b0a8e76c42f8956571b52fd1f7af97c
Trevor Rounce <trevorrounce@Trevors-iMac.hitronhub.home>
2019-06-14 10:30:21 -0600

    added the schem rich snippets for SEO

--- src/components/Head/seo.js
+++ src/components/Head/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import SchemaOrg from "./SchemaOrg"
 
 function SEO({ description, lang, meta, metaImage, keywords, title }) {
   const { site } = useStaticQuery(
@@ -30,76 +31,90 @@ function SEO({ description, lang, meta, metaImage, keywords, title }) {
   const image = metaImage ? `${site.siteMetadata.siteUrl}${metaImage}` : null
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          property: `og:locale`,
-          content: `en_US`,
-        },
-        {
-          property: `og:url`,
-          content: `https://www.nosecreekvalleymuseum.com/`,
-        },
-        {
-          property: `og:site_name`,
-          content: `Nose Creek Valley Museum`,
-        },
-        {
-          property: "og:image",
-          content: image,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-        {
-          name: "google-site-verification",
-          content: "-FHzs0DuBu3_NEc_D_NfwSDmqz0uxA9frLHh67MclLA",
-        },
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
-        )
-        .concat(meta)}
-    />
+    <React.Fragment>
+      <Helmet
+        htmlAttributes={{
+          lang,
+        }}
+        title={title}
+        titleTemplate={`%s | ${site.siteMetadata.title}`}
+        meta={[
+          {
+            name: `description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:title`,
+            content: title,
+          },
+          {
+            property: `og:description`,
+            content: metaDescription,
+          },
+          {
+            property: `og:type`,
+            content: `website`,
+          },
+          {
+            property: `og:locale`,
+            content: `en_US`,
+          },
+          {
+            property: `og:url`,
+            content: `https://www.nosecreekvalleymuseum.com/`,
+          },
+          {
+            property: `og:site_name`,
+            content: `Nose Creek Valley Museum`,
+          },
+          {
+            property: "og:image",
+            content: image,
+          },
+          {
+            name: `twitter:card`,
+            content: `summary`,
+          },
+          {
+            name: `twitter:creator`,
+            content: site.siteMetadata.author,
+          },
+          {
+            name: `twitter:title`,
+            content: title,
+          },
+          {
+            name: `twitter:description`,
+            content: metaDescription,
+          },
+          {
+            name: "google-site-verification",
+            content: "-FHzs0DuBu3_NEc_D_NfwSDmqz0uxA9frLHh67MclLA",
+          },
+        ]
+          .concat(
+            keywords.length > 0
+              ? {
+                  name: `keywords`,
+                  content: keywords.join(`, `),
+                }
+              : []
+          )
+          .concat(meta)}
+      />
+      <SchemaOrg
+        isBlogPost={false}
+        url={site.siteMetadata.siteUrl}
+        title={title}
+        image={image}
+        description={description}
+        datePublished="June 14, 2019"
+        canonicalUrl="https://nosecreekvalleymuseum.com"
+        author="Nose Creek Valley Museum"
+        organization="Nose Creek Valley Museum"
+        defaultTitle="Nose Creek Valley Museum"
+      />
+    </React.Fragment>
   )
 }
 
