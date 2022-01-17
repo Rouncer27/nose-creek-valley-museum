82c1be5253cb32a9abdc493a44ebf24b9399cf39
Trevor <trevor@switchbackcreative.ca>
2022-01-17 11:10:49 -0700

    upercased the FAQ title

--- src/components/Pages/About/Faqs.js
+++ src/components/Pages/About/Faqs.js
@@ -169,7 +169,7 @@ class Faqs extends Component {
       <FaqsStyled id="faqsInfo" className="faqs">
         <StandardWrapper className="faqs__wrapper">
           <div className="faqs__title">
-            <h2>Faq</h2>
+            <h2>FAQ</h2>
           </div>
           <div className="faqs__container">
             {this.props.faqs.map((faq, index) => {
